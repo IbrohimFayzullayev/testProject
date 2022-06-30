e5aef4252622c483fce1bd7bccb108cca269cfae
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-06-30 11:59:25 +0500

    project finished

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div class="card" style={{ width: "18rem" }}>
+      <img src="..." class="card-img-top" alt="..." />
+      <div class="card-body">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <button type="button" class="btn btn-success">
+        Primary
+      </button>
+    </div>
+  );
+};
+
+export default Card;
